09d1beccd531722bcad97c4e281b5fabae8fa29b
pkesavulu <pkesavulu.91@gmail.com>
2022-10-17 19:48:39 +0530

    removed new changes

--- app/src/main/java/com/example/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/fcm/MyFirebaseMessagingService.java
@@ -31,42 +31,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String CHANNEL_DESC = "Firebase Cloud Messaging";
     private int numMessages = 0;
 
-//    @Override
-//    public void onMessageReceived(RemoteMessage remoteMessage) {
-//        super.onMessageReceived(remoteMessage);
-//        RemoteMessage.Notification notification = remoteMessage.getNotification();
-//        Map<String, String> data = remoteMessage.getData();
-//        Log.d("FROM", remoteMessage.getFrom());
-//        sendNotification(notification, data);
-//    }
-
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: " + remoteMessage.getFrom());
-
-        // Check if message contains a data payload.
-        if (remoteMessage.getData().size() > 0) {
-            Log.d(TAG, "Message data payload: " + remoteMessage.getData());
-
-          /*  if (*//* Check if data needs to be processed by long running job *//* true) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-                scheduleJob();
-            } else {
-                // Handle message within 10 seconds
-                handleNow();
-            }*/
-
-        }
-
-        // Check if message contains a notification payload.
-        if (remoteMessage.getNotification() != null) {
-            Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-        }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
+        super.onMessageReceived(remoteMessage);
+        RemoteMessage.Notification notification = remoteMessage.getNotification();
+        Map<String, String> data = remoteMessage.getData();
+        Log.d("FROM", remoteMessage.getFrom());
+        sendNotification(notification, data);
     }
 
     private void sendNotification(RemoteMessage.Notification notification, Map<String, String> data) {
